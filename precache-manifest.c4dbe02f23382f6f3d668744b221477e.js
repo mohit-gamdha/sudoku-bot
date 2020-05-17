5abc9a4542b9919a993bbdb0ef2fd3375122278e
Mohit Gamdha <mohit.gamdha@byteprophecy.com>
2020-05-17 14:00:52 +0530

    Updates

---
+++ precache-manifest.c4dbe02f23382f6f3d668744b221477e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0510cf9750951d20dcb5e90778ede1",
+    "url": "/sudoku-bot/index.html"
+  },
+  {
+    "revision": "9e4b47f9373631a56f99",
+    "url": "/sudoku-bot/static/css/main.7bcfaf7d.chunk.css"
+  },
+  {
+    "revision": "3ce20dffd6a664f0543f",
+    "url": "/sudoku-bot/static/js/2.3f38a72f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sudoku-bot/static/js/2.3f38a72f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e4b47f9373631a56f99",
+    "url": "/sudoku-bot/static/js/main.11258f14.chunk.js"
+  },
+  {
+    "revision": "0752685805d6a34dd9e9",
+    "url": "/sudoku-bot/static/js/runtime-main.60c38569.js"
+  }
+]);
